a695eea7c5b7522cb77487de0e10b86b96ba55b4
OmarAliSoliman <omar.ali.alasmar@gmail.com>
2020-06-27 13:58:15 +0200

    edit4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,6 @@
 $(document).ready(function(){
+  
   AOS.init();
-  // var swiper = new Swiper('.swiper-slider-card', {
-  //   slidesPerView: 1,
-  //   spaceBetween:0,
-  //   // init: false,
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  //   breakpoints: {
-  //     640: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 0,
-  //     },
-  //     768: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 0,
-  //     },
-  //     1024: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 0,
-  //     },
-  //   }
-  // });
 
   $('.slider-department-card').slick({
     dots: false,
